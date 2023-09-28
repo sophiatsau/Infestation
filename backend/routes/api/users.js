2a770e536a769320053c0fce9245522f3b0007f5
Sophia <sophia.tsau@gmail.com>
2023-09-28 16:50:58 -0700

    add user input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,10 +5,38 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+//used to validate request bodies for signup
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+/******************* MIDDLEWARE *************** */
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+
+/***************** ROUTE HANDLERS *********** */
+
 const router = express.Router();
 
 // Signup: POST /api/users
-router.post('/', async(req,res,next) => {
+router.post('/', validateSignup, async(req,res,next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcrypt.hashSync(password);
 
