3b04ce0df9dd8cb639abc21870dc2771f8a8ab90
Sophia <sophia.tsau@gmail.com>
2023-09-30 16:45:05 -0700

    removed stack trace for errors in production

--- backend/app.js
+++ backend/app.js
@@ -79,12 +79,16 @@ app.use((err, _req, _res, next) => {
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
-  res.json({
+
+  const errBody = {
     title: err.title || 'Server Error',
     message: err.message,
     errors: err.errors,
-    stack: isProduction ? null : err.stack
-  });
+  }
+
+  if (!isProduction) errBody.stack = err.stack;
+
+  res.json(errBody);
 });
 
 module.exports = app;
