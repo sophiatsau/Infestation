aea50dc8c1f51ff6bfbba8fcd0097e38fb9f2bda
Sophia <sophia.tsau@gmail.com>
2023-10-02 10:32:42 -0700

    refactored code for group routes and model

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -1,9 +1,53 @@
 'use strict';
 const {
-  Model
+  Model, Op
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Group extends Model {
+
+    static async findCountMembersGetPreview(queryObj) {
+      const {numMembers, previewImage, currentUser} = queryObj;
+
+      const {Membership, GroupImage} = require('../models')
+      const include = [];
+      const where = {};
+      if (numMembers) {
+        include.push({
+            model: Membership,
+            group: ['groupId'],
+        })
+      }
+
+      if (previewImage) {
+        include.push({
+          model: GroupImage,
+          where: {
+              preview: true,
+          },
+          attributes: ['url'],
+        })
+      }
+
+      //can concat separate query for memberships?
+      if (currentUser) {
+        where.where = {
+          organizerId: currentUser,
+        }
+      }
+
+      let groups = await Group.findAll({include, ...where});
+
+      groups = groups.map( group => {
+          group = group.toJSON();
+          group.numMembers = group.Memberships.length;
+          group.previewImage = group.GroupImages[0].url || "Preview not found";
+          delete group.Memberships;
+          delete group.GroupImages;
+          return group
+      })
+
+      return groups
+    }
 
     static associate(models) {
       Group.belongsTo(models.User, {
--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,6 +1,7 @@
 // backend/routes/api/session.js
 const express = require('express');
 const {Op} = require('sequelize');
+const Sequelize = require('sequelize')
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
@@ -41,37 +42,38 @@ const router = express.Router();
 //Returns all the groups.
 //authenticate: false
 router.get('/', async (req,res,next) => {
-    const query = {
-        include: [
-            {
-                model: Membership,
-                group: ['groupId'],
-                // attributes: [[sequelize.fn('COUNT',sequelize.col('Memberships.groupId')), 'numMembers'],],
-            },
-            {
-                model: GroupImage,
-                where: {
-                    preview: true,
-                },
-                attributes: ['url'],
-            }
-        ],
-    }
+    // const query = {
+    //     include: [
+    //         {
+    //             model: Membership,
+    //             group: ['groupId'],
+    //             // attributes: [[sequelize.fn('COUNT',sequelize.col('Memberships.groupId')), 'numMembers'],],
+    //             //where: {groupId: this.id}
+    //         },
+    //         {
+    //             model: GroupImage,
+    //             where: {
+    //                 preview: true,
+    //             },
+    //             attributes: ['url'],
+    //         }
+    //     ],
+    // }
 
-    //include num members
-    //include preview images
-    let groups = await Group.findAll(query);
+    // //include num members
+    // //include preview images
+    // let groups = await Group.findAll(query);
 
-    groups = groups.map( group => {
-        group = group.toJSON();
-        group.numMembers = group.Memberships.length;
-        group.previewImage = group.GroupImages[0].url || "Preview not found";
-        delete group.Memberships;
-        delete group.GroupImages;
-        return group
-    })
+    // groups = groups.map( group => {
+    //     group = group.toJSON();
+    //     group.numMembers = group.Memberships.length;
+    //     group.previewImage = group.GroupImages[0].url || "Preview not found";
+    //     delete group.Memberships;
+    //     delete group.GroupImages;
+    //     return group
+    // })
 
-    res.json({Groups: groups});
+    res.json({Groups: await Group.findCountMembersGetPreview({numMembers: true, previewImage: true})});
 })
 
 //Get all Groups joined or organized by the Current User
@@ -82,8 +84,10 @@ router.get('/current', requireAuth, async (req,res,next) => {
     //where organizerId = current user's id
     // OR where Membership.userId = groups, groupId = users
     // make membership into join table!
-    const groups = await Group.findAll();
-    res.json({Groups: groups});
+    const userId = req.user.id;
+    // const groups = await Group.findCountMembersGetPreview({numMembers: true, previewImage: true, currentUser: userId});
+
+    res.json({Groups: await Group.findCountMembersGetPreview({numMembers: true, previewImage: true, currentUser: userId})});
 })
 
 //Get details of a Group from an id
