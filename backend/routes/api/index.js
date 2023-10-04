966ba77045a67d3efa78634ece4fc1ab13444457
Sophia <sophia.tsau@gmail.com>
2023-10-04 10:32:46 -0700

    attached router for membership feature

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const usersRouter = require('./users');
 const groupsRouter = require('./groups');
 const venueRouter = require('./venues');
 const eventRouter = require('./events');
+const membershipRouter = require('./memberships');
 
 const { restoreUser } = require('../../utils/auth.js');
 
@@ -18,6 +19,7 @@ router.use('/users', usersRouter);
 router.use('/groups', groupsRouter);
 router.use('/venues', venueRouter);
 router.use('/events', eventRouter);
+router.use('/memberships', membershipRouter);
 
 router.post('/test', function(req,res) {
     res.json({requestBody: req.body});
