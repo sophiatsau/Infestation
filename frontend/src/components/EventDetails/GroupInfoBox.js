2ece6a6c01e240ffcb73014d7f58fb4b82e1f98d
Sophia <sophia.tsau@gmail.com>
2023-10-24 13:58:38 -0700

    added group info box to event details page

--- frontend/src/components/EventDetails/GroupInfoBox.js
+++ frontend/src/components/EventDetails/GroupInfoBox.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function GroupInfoBox({group}) {
+  console.log("🚀 ~ file: GroupInfoBox.js:4 ~ GroupInfoBox ~ group:", group)
+  const {name, GroupImages, isPrivate, id} = group;
+  const previewImage = GroupImages && GroupImages.find(image => image.preview);
+
   return (
-    <div>GroupInfoBox</div>
+    <Link to={`/groups/${id}`}>
+      <img src={previewImage?.url}/>
+      <div>
+        <h3>{name}</h3>
+        <p>{isPrivate}</p>
+      </div>
+    </Link>
   )
 }
