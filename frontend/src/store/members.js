67f71d5ab7688aaa3c53dd217175212f846ff7cc
Sophia <sophia.tsau@gmail.com>
2024-02-02 23:30:22 -0800

    constants for member store

--- frontend/src/store/members.js
+++ frontend/src/store/members.js
@@ -1,10 +1,17 @@
 import { csrfFetch } from "./csrf";
+import { REMOVE_USER } from "./session";
 
+const GET_GROUP_MEMBERS = 'members/getGroupMembers'
+const REQUEST_MEMBERSHIP = 'members/requestMembership'
+const UPDATE_MEMBERSHIP = 'members/updateMembership'
+const DELETE_MEMBERSHIP = 'members/deleteMembership'
 
 const initialState = {}
 
 const membersReducer = (state=initialState, action) => {
     switch (action.type) {
+        case REMOVE_USER:
+            return initialState
         default:
             return state
     }
