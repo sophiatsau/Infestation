d35e57ee271ffc570409cfc51000800608c16078
Sophia <sophia.tsau@gmail.com>
2024-02-02 23:26:02 -0800

    members slice in store

---
+++ frontend/src/store/members.js
@@ -0,0 +1,13 @@
+import { csrfFetch } from "./csrf";
+
+
+const initialState = {}
+
+const membersReducer = (state=initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default membersReducer
