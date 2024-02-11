4a035867f02fef88e01225dbbc0617215a0c05dd
Sophia <sophia.tsau@gmail.com>
2024-02-11 05:14:52 -0800

    show "no members" if no members in group

--- frontend/src/components/GroupDetails/ViewMembershipsModal.js
+++ frontend/src/components/GroupDetails/ViewMembershipsModal.js
@@ -1,20 +1,22 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { thunkGetGroupMembers } from '../../store/members'
 
 export default function ViewMembershipsModal({groupId}) {
     const memberships = useSelector(state => state.members)
+    const [isLoaded, setIsLoaded] = useState(false)
     const dispatch = useDispatch()
 
     useEffect(() => {
-        dispatch(thunkGetGroupMembers(groupId))
-    }, [memberships, dispatch])
+        dispatch(thunkGetGroupMembers(groupId)).then(()=>setIsLoaded(true))
+    }, [memberships, dispatch, setIsLoaded])
 
     return (
     <>
         <h1>Members</h1>
         {
-            !Object.values(memberships).length ? "Loading memberships..."
+            !isLoaded ? "Loading memberships..."
+            : !Object.values(memberships).length ? "There are no members in this group"
             : Object.values(memberships).map(membership =>(
                 <div key={membership.id}>
                     <span>{membership.firstName} {membership.lastName}</span> <span>{membership.Membership.status}</span>
