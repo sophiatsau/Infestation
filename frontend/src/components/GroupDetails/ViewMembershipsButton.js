8f50dfbb6dc1b392dc18d06b180d2776585a35de
Sophia <sophia.tsau@gmail.com>
2024-03-11 18:26:52 -0700

    remove view memberships button if user is not logged in

--- frontend/src/components/GroupDetails/ViewMembershipsButton.js
+++ frontend/src/components/GroupDetails/ViewMembershipsButton.js
@@ -2,8 +2,12 @@ import React from 'react'
 import OpenModalButton from '../OpenModalButton'
 
 import ViewMembershipsModal from './ViewMembershipsModal'
+import { useSelector } from 'react-redux'
 
 export default function ViewMembershipsButton({groupId}) {
+  const user = useSelector(state => state.session.user)
+
+  if (!user) return null
 
   return (
     <OpenModalButton
