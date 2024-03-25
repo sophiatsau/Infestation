3a7238aedc72945b3877a4fbc2e9eac4fd1ee754
Sophia <sophia.tsau@gmail.com>
2024-03-25 11:24:29 -0700

    intermit commit

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div id="footer">
+        <div className='footer-icon-links'>
+            <a href="https://github.com/sophiatsau">
+                <img className="footer-icon" src="./images/github-logo.png" style={{height:"20px",width:"20px"}} alt="github icon"/>
+            </a>
+            Sophia Tsau
+            <a href="https://www.linkedin.com/in/sophiatsau/">
+                <img className="footer-icon" src="./images/linkedin-logo.png" style={{height:"20px",width:"20px"}} alt="linkedin icon"/>
+            </a>
+        </div>
+        <div>© 2023 Crittr, Inc. All Rights Reserved. Terms, Privacy & Accessibility</div>
+    </div>
+  )
+}
