f1ba507df44195270afd35e498627c7569daccb6
Sophia <sophia.tsau@gmail.com>
2024-03-25 11:34:56 -0700

    icons from aws bucket

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,11 +6,11 @@ export default function Footer() {
     <div id="footer">
         <div className='footer-icon-links'>
             <a href="https://github.com/sophiatsau">
-                <img className="footer-icon" src="./images/github-logo.png" style={{height:"20px",width:"20px"}} alt="github icon"/>
+                <img className="footer-icon" src="https://crittr-images.s3.us-west-1.amazonaws.com/github.png" alt="github icon"/>
             </a>
             Sophia Tsau
             <a href="https://www.linkedin.com/in/sophiatsau/">
-                <img className="footer-icon" src="./images/linkedin-logo.png" style={{height:"20px",width:"20px"}} alt="linkedin icon"/>
+                <img className="footer-icon" src="https://crittr-images.s3.us-west-1.amazonaws.com/LinkedIn_icon_circle.svg.png" alt="linkedin icon"/>
             </a>
         </div>
         <div>© 2023 infestation, Inc. All Rights Reserved. Terms, Privacy & Accessibility</div>
