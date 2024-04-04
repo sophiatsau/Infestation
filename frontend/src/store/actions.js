ef561678d998b6340dcfb4481754f719f03a5f65
Sophia <sophia.tsau@gmail.com>
2024-04-03 19:14:19 -0700

    create group automatically gives organizer the "co-host" status

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -2,3 +2,6 @@ export const GET_GROUP_MEMBERS = 'members/getGroupMembers'
 export const REQUEST_MEMBERSHIP = 'members/requestMembership'
 export const UPDATE_MEMBERSHIP = 'members/updateMembership'
 export const DELETE_MEMBERSHIP = 'members/deleteMembership'
+
+export const CREATE_GROUP = 'groups/createGroup';
+export const DELETE_GROUP = 'groups/deleteGroup'
