c688e51d948b89e0349db14582da9ddae8198d5d
Sophia <sophia.tsau@gmail.com>
2024-07-24 14:48:51 -0700

    styling for approving memberships

--- frontend/src/components/GroupDetails/MembershipCard.js
+++ frontend/src/components/GroupDetails/MembershipCard.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 
 export default function MembershipCard({membership, approveMembership, authorized, isOrganizer, deleteMembership, makeCoHost}) {
     const [openMenu, setOpenMenu] = useState(false)
-    const ulRef = useRef()
+    const divRef = useRef()
     const user = useSelector(state => state.session.user)
     // const group = useSelector(state => state.groups.currentGroup)
     // const isOrganizer = user.id === group.organizerId
@@ -13,8 +13,8 @@ export default function MembershipCard({membership, approveMembership, authorize
         if (!openMenu) return
 
         const closeMenu = (e) => {
-            // account also for clicking outside of modal (ulRef.current === null)
-            if (!ulRef.current || ulRef.current.contains(e.target)) return
+            // account also for clicking outside of modal (divRef.current === null)
+            if (!divRef.current || divRef.current.contains(e.target)) return
             setOpenMenu(false)
         }
 
@@ -24,18 +24,18 @@ export default function MembershipCard({membership, approveMembership, authorize
 
     const toggleMenu = () => setOpenMenu(!openMenu)
 
-    const menuClass = openMenu ? "dropdown" : "hidden"
+    const menuClass = openMenu ? "modal-table-dropdown" : "hidden"
 
     const dropdownOptions = (
         <>
             <button className='light-button' onClick={toggleMenu}>
                 <i className="fa-solid fa-ellipsis"></i>
             </button>
-            <ul className={menuClass} ref={ulRef}>
+            <div className={menuClass} ref={divRef}>
                 {isOrganizer && membership.Membership.status ==="member" &&
-                <li><button className='light-button' onClick={makeCoHost(membership, toggleMenu)}>Make Co-Host</button></li>}
-                <li><button className='light-button' onClick={deleteMembership(membership)}>Remove Member</button></li>
-            </ul>
+                <button style={{margin:0}} className='light-button' onClick={makeCoHost(membership, toggleMenu)}>Make Co-Host</button>}
+                <button style={{margin:0}} className='light-button' onClick={deleteMembership(membership)}>Remove Member</button>
+            </div>
         </>
     )
 
@@ -46,7 +46,7 @@ export default function MembershipCard({membership, approveMembership, authorize
             <div className='grey lighter small'>{membership.Membership.status}</div>
         </td>
         <td>
-            {membership.Membership.status === "pending" && <button onClick={approveMembership(membership)}>Approve</button>}
+            {membership.Membership.status === "pending" && <button style={{margin:0}} onClick={approveMembership(membership)}>Approve</button>}
         </td>
         <td>
             {isOrganizer && membership.id !== user.id && dropdownOptions}
