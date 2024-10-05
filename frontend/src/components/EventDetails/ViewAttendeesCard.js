b36befc7f34966007e2f43075f241ecad1add5b9
Sophia <sophia.tsau@gmail.com>
2024-10-05 00:29:05 -0700

    intermit commit: menu for changing attendance status

--- frontend/src/components/EventDetails/ViewAttendeesCard.js
+++ frontend/src/components/EventDetails/ViewAttendeesCard.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 
 export default function ViewAttendeesCard({attendee, isCoHost}) {
-  const canApprove = attendee.Attendance.status==="pending" && isCoHost
+  const [openMenu, setOpenMenu] = useState(false)
+  const menuRef = useRef()
+  const menuClass = openMenu ? "modal-table-dropdown" : "hidden"
+
+  const canApprove = attendee.Attendance.status!=="attending" && isCoHost
+
+  const toggleMenu = () => setOpenMenu(!openMenu)
+
+  const approveAttendance = () => {
+    return
+  }
+
+  // when pressed, open dropdown menu
+  // dropdown menu: has options. PENDING: move to waitlist, move to attending. WAITLIST: move to attending. ATTENDING: move to waitlist
+  const approveAttendanceButton = <button onClick={toggleMenu}>Actions</button>
 
   return (<>
     <td>{attendee.firstName} {attendee.lastName}</td>
@@ -10,7 +24,14 @@ export default function ViewAttendeesCard({attendee, isCoHost}) {
     </td>
     <td>
       {/* ACCEPT PENDING, WAITLIST > ATTENDING for co-host of group */}
-      {canApprove && "Approve"}
+      {canApprove &&
+      <>
+        {approveAttendanceButton}
+        <div class={menuClass} ref={menuRef}>
+          the menu
+        </div>
+      </>
+      }
     </td>
     <td>
       {/* Delete attendance for co-host of group*/}
